d88bd8e2ee0f165ecec596f5de9dbc0e17c36010
Victor Henrique Lage Santos <victor.lage@external.stellantis.com>
2024-10-02 20:30:39 -0300

    Adding radius filter

--- generate_2d_countor.py
+++ generate_2d_countor.py
@@ -5,7 +5,24 @@ from numpy.typing import NDArray
 from sklearn.cluster import DBSCAN
 
 from functions.algebric_functions import calculate_angle
-from generate_raster import two_d_section
+
+
+def two_d_section(solid_mesh, z_val):
+
+    plane = pv.Plane(
+        center=(0, 0, z_val),
+        direction=(0, 0, 1),
+        i_size=500,
+        j_size=500,
+        i_resolution=150,
+        j_resolution=150
+    ).triangulate()
+
+    intersection, _, _ = solid_mesh.intersection(plane) # type: ignore
+
+    intersec_points = intersection.points
+    return intersec_points
+
 
 def find_next_point(
         current_point: NDArray[floating],
@@ -23,7 +40,7 @@ def find_next_point(
 
     sorted_indices = np.argsort(distances)
 
-    for idx in sorted_indices[1:]:
+    for idx in sorted_indices:
         candidate_point = points[idx]
         candidate_vector = candidate_point - current_point
 
@@ -38,7 +55,7 @@ def find_next_point(
 
     return None
 
-def connect_points(points: NDArray[floating]) -> NDArray[floating]:
+def connect_points(points: NDArray[floating], min_radius: float) -> NDArray[floating]:
     """
     Connects points in a sequence starting from the point with the minimum y-coordinate.
     The next point is determined by the closest point that satisfies the angle constraint.
@@ -47,9 +64,11 @@ def connect_points(points: NDArray[floating]) -> NDArray[floating]:
     current_point = points[np.argmin(points[:, 1])]
 
     path = [current_point]
-
+    # import matplotlib.pyplot as plt
     previous_vector = None
-    while len(path) < len(points):
+    while len(points) > 0:
+        distances = np.linalg.norm(points - current_point, axis=1)
+        points = points[distances > min_radius]
         next_point = find_next_point(current_point, points, previous_vector)
         if next_point is None:
             break
@@ -63,6 +82,8 @@ def connect_points(points: NDArray[floating]) -> NDArray[floating]:
 if __name__ == "__main__":
     INSOLE_FILE_PATH = r'output_files\insole.stl'
     Z_VAL = 1
+    MIN_RADIUS = 1
+
     mesh = pv.read(INSOLE_FILE_PATH)
 
     intersection_points = two_d_section(mesh, Z_VAL)
@@ -74,9 +95,9 @@ if __name__ == "__main__":
     labels = dbscan.fit_predict(points_2d)
 
     contours = []
-    for label in np.unique(labels[labels != -1]):
+    for label in np.unique(labels[(labels != -1) & (labels != 0)]):
         cluster_points = points_2d[labels == label]
-        paths = connect_points(cluster_points)
+        paths = connect_points(cluster_points, MIN_RADIUS)
         contours.append(paths)
 
 
@@ -94,6 +115,7 @@ if __name__ == "__main__":
 
             lines[i] = [[start[0], start[1], Z_VAL], [end[0], end[1], Z_VAL]]
 
-        pl.add_lines(np.array(lines).reshape(-1, 3), color='black', width=10)
+        pl.add_lines(np.array(lines).reshape(-1, 3), color='blue', width=5)
+        pl.add_mesh(pv.PolyData(lines[:, 0]), color='black', point_size=8)
 
     pl.show()
