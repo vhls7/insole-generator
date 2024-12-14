4a05908df63af72a5b9262643e7f3f54d481eb83
Victor Henrique Lage Santos <victor.lage@external.stellantis.com>
2024-12-14 12:30:44 -0300

    Fazendo o botão ficar desabilitado antes de selecionar todas as opções

--- QT_DESIGN/main.py
+++ QT_DESIGN/main.py
@@ -414,12 +414,27 @@ class SelectBases(QtWidgets.QMainWindow):
     flag_insert='false'
     def __init__(self):
         super().__init__()
+
+        self.base_name = None
+
         # Loading interface developed in Qt Designer
         uic.loadUi(r"select_base.ui", self)
 
-        # Connecting button to function
-        select_base_button = self.findChild(QtWidgets.QPushButton, "btn_inserir_base")
-        select_base_button.clicked.connect(self.load_parametric_base)
+        # region Connecting buttons to functions
+        self.select_base_button = self.findChild(QtWidgets.QPushButton, "btn_inserir_base")
+        self.select_base_button.clicked.connect(self.load_parametric_base)
+
+        self.cb_sizing = self.findChild(QtWidgets.QComboBox, "CB_numeracao")
+        self.cb_sizing.currentTextChanged.connect(self.update_button_state)
+    
+        self.cb_thickness = self.findChild(QtWidgets.QComboBox, "CB_espessura")
+        self.cb_thickness.currentTextChanged.connect(self.update_button_state)
+
+        self.cb_height = self.findChild(QtWidgets.QComboBox, "CB_calcanhar")
+        self.cb_height.currentTextChanged.connect(self.update_button_state)
+        # endregion
+
+        self.update_button_state()
 
     def closeEvent(self, event):
         # Emite o sinal ao fechar a janela, passando a string desejada
@@ -428,28 +443,25 @@ class SelectBases(QtWidgets.QMainWindow):
         event.accept()
 
     def load_parametric_base(self):
-        cb_numeracao = self.findChild(QtWidgets.QComboBox, "CB_numeracao")
-        cb_espessura = self.findChild(QtWidgets.QComboBox, "CB_espessura")
-        cb_altura = self.findChild(QtWidgets.QComboBox, "CB_calcanhar")
-        num = cb_numeracao.currentText()
-        esp = cb_espessura.currentText()
-        alt = cb_altura.currentText()
-
-        if num == 'Escolha uma opção':
-            num = 0
-            return
-        if(esp == 'Escolha uma opção'):
-            esp = 3
-        if(alt == 'Escolha uma opção'):
-            alt = 1
-                
+        num = self.cb_sizing.currentText()
+        esp = self.cb_thickness.currentText()
+        alt = self.cb_height.currentText()
 
         self.base_name = f'PAMLILHAS_STL/{num}/BASES/CONTATO_{esp}_{alt}.STL'
         self.flag_insert='true'
 
         self.close()
 
+    def update_button_state(self):
+        default_text = "Escolha uma opção"
 
+        # Check if all ComboBoxes have a valid selection
+        is_valid_num = self.cb_sizing.currentText() != default_text
+        is_valid_esp = self.cb_thickness.currentText() != default_text
+        is_valid_alt = self.cb_height.currentText() != default_text
+
+        # Enables or disables the button based on conditions
+        self.select_base_button.setEnabled(is_valid_num and is_valid_esp and is_valid_alt)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
